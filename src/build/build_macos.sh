db6206ec141dc572f40171a174818af09c484d87
Francesco Servida <git.fservida@gmail.com>
2024-02-23 14:51:50 +0300

    Initial changes for codesigning on macOS

--- src/build/build_macos.sh
+++ src/build/build_macos.sh
@@ -1,6 +1,6 @@
 pip install Pillow
 rm -rf build/
-pyinstaller src/main/python/main.py --workpath build/build --distpath build/dist/app --clean --osx-bundle-identifier ch.francescoservida.gemino --windowed --icon src/main/icons/mac/256.png -y -n gemino
+pyinstaller src/main/python/main.py --workpath build/build --distpath build/dist/app --clean --osx-bundle-identifier ch.francescoservida.gemino --codesign-identity "Developer ID Application: Francesco Servida (UVXFW83BXV)" --windowed --icon src/main/icons/mac/256.png -y -n gemino
 rm -rf build/dist/app/gemino
 git clone https://github.com/create-dmg/create-dmg.git
 create-dmg/create-dmg --volname gemino --app-drop-link 10 10 build/dist/gemino.dmg build/dist/app
