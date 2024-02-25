45a06db9b8a72990ac250293162d248f21bc5490
Francesco Servida <git.fservida@gmail.com>
2024-02-25 19:45:12 +0300

    Implemented Notarization for macOS on CI

--- src/build/build_macos.sh
+++ src/build/build_macos.sh
@@ -2,5 +2,42 @@ pip install Pillow
 rm -rf build/
 pyinstaller src/main/python/main.py --workpath build/build --distpath build/dist/app --clean --osx-bundle-identifier ch.francescoservida.gemino --codesign-identity "Developer ID Application: Francesco Servida (UVXFW83BXV)" --windowed --icon src/main/icons/mac/256.png -y -n gemino
 rm -rf build/dist/app/gemino
+
+# Store the notarization credentials so that we can prevent a UI password dialog
+# from blocking the CI
+
+echo "Create keychain profile"
+echo $MACOS_NOTARIZATION_KEY | base64 --decode > authkey.p8
+xcrun notarytool store-credentials -k authkey.p8 -d $MACOS_NOTARIZATION_KEY_ID -i $MACOS_NOTARIZATION_ISSUER
+rm -P authkey.p8
+
+# We can't notarize an app bundle directly, but we need to compress it as an archive.
+# Therefore, we create a zip file containing our app bundle, so that we can send it to the
+# notarization service
+
+echo "Creating temp notarization archive"
+ditto -c -k --keepParent "build/dist/app/gemino.app" "notarization.zip"
+
+# Here we send the notarization request to the Apple's Notarization service, waiting for the result.
+# This typically takes a few seconds inside a CI environment, but it might take more depending on the App
+# characteristics. Visit the Notarization docs for more information and strategies on how to optimize it if
+# you're curious
+
+echo "Notarize app"
+xcrun notarytool submit "notarization.zip" --keychain-profile "notarytool-profile" --wait
+
+# Finally, we need to "attach the staple" to our executable, which will allow our app to be
+# validated by macOS even when an internet connection is not available.
+echo "Attach staple"
+xcrun stapler staple "build/dist/app/gemino.app"
+
 git clone https://github.com/create-dmg/create-dmg.git
 create-dmg/create-dmg --volname gemino --app-drop-link 10 10 build/dist/gemino.dmg build/dist/app
+codesign -fvs "Developer ID Application: Francesco Servida (UVXFW83BXV)" build/dist/gemino.dmg
+
+
+xcrun notarytool submit build/dist/gemino.dmg --keychain-profile "notarytool-profile" --wait
+# Finally, we need to "attach the staple" to our dmg, which will allow the image to be
+# validated by macOS even when an internet connection is not available.
+echo "Attach staple"
+xcrun stapler staple "build/dist/gemino.dmg"
