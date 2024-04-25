44b42f05baedd9ff07cebbbf1e37b0e79e8f48aa
Francesco Servida <git.fservida@gmail.com>
2024-04-25 19:37:02 +0300

    Reverted Viewer - Removed QWebView due to issues with macos packaging. Support for PDF with QPDFView, no more support for DOCX files

--- src/main/python/gemino/view/advanced.py
+++ src/main/python/gemino/view/advanced.py
@@ -4,10 +4,10 @@ import traceback
 from typing import Union
 import puremagic
 
-from PySide6 import QtWidgets, QtCore, QtGui
+from PySide6 import QtWidgets, QtCore, QtGui, QtPdf
+from PySide6.QtPdfWidgets import QPdfView
 from pathlib import Path
 
-from PySide6.QtWebEngineWidgets import QWebEngineView
 from puremagic import PureError
 from pyaff4.container import (
     PhysicalImageContainer,
@@ -19,7 +19,6 @@ from pyaff4.container import (
 
 from pyaff4 import lexicon, rdfvalue
 
-from .viewer_libraries import PDF_JS_MIN, PDF_JS_MIN_WORKER, JSZIP_JS_MIN, DOCX_JS_MIN
 from ..copy.logical.aff4 import AFF4Item
 
 
@@ -160,13 +159,13 @@ class AdvancedWidget(QtWidgets.QWidget):
         self.text_edit = QtWidgets.QTextEdit()
         self.text_edit.setReadOnly(True)  # Set readonly
 
-        ## Web engine view for PDF and office documents
-        self.web_view = QWebEngineView()
+        ## PDF View
+        self.pdf_view = QPdfView()
+        self.pdf_view.setPageMode(QPdfView.PageMode.MultiPage)
 
-        # Initially hide all widgets
+        # Initially hide all widgets, Keep text edit always to keep layout uniform
         self.image_label.hide()
-        # self.text_edit.hide() # Keep text edit always to keep layout uniform
-        self.web_view.hide()
+        self.pdf_view.hide()
 
         ## Layout Management
         self.window_layout = QtWidgets.QVBoxLayout()
@@ -183,7 +182,7 @@ class AdvancedWidget(QtWidgets.QWidget):
         self.viewer_layout.addWidget(self.hex_viewer)
         self.viewer_layout.addWidget(self.image_label)
         self.viewer_layout.addWidget(self.text_edit)
-        self.viewer_layout.addWidget(self.web_view)
+        self.viewer_layout.addWidget(self.pdf_view)
         self.viewer_tab.setLayout(self.viewer_layout)
 
         # Layout and Widget shenanigans to ensure it matches margins of viewer tab
@@ -237,7 +236,7 @@ class AdvancedWidget(QtWidgets.QWidget):
         self.hex_viewer.reset()
         self.image_label.clear()
         self.text_edit.clear()
-        self.web_view.setHtml("")
+        self.pdf_view.setDocument(None)
         self.metadata_box.clear()
 
         self.container_label.setText(src_container_path)
@@ -307,11 +306,11 @@ class AdvancedWidget(QtWidgets.QWidget):
         self.hex_viewer.clear()
         self.image_label.clear()
         self.text_edit.clear()
-        self.web_view.setHtml("")
+        self.pdf_view.setDocument(None)
         self.metadata_box.clear()
         self.image_label.hide()
         self.text_edit.hide()
-        self.web_view.hide()
+        self.pdf_view.hide()
         self.export_button.setDisabled(True)
         self.load_metadata(urn, folder)
         if folder:
@@ -472,10 +471,10 @@ class AdvancedWidget(QtWidgets.QWidget):
         # Hide all widgets
         self.image_label.clear()
         self.text_edit.clear()
-        self.web_view.setHtml("")
+        self.pdf_view.setDocument(None)
         self.image_label.hide()
         self.text_edit.hide()
-        self.web_view.hide()
+        self.pdf_view.hide()
 
         try:
             # Display content based on MIME type
@@ -497,187 +496,27 @@ class AdvancedWidget(QtWidgets.QWidget):
 
                 self.image_label.setPixmap(pixmap)
                 self.image_label.show()
-                # image_data = base64.b64encode(data).decode('utf-8')
-                # html = (
-                #         """
-                #             <!DOCTYPE html>
-                #             <html>
-                #             <head>
-                #                 <title>Image Viewer</title>
-                #                 <meta charset="UTF-8">
-                #                 <style>
-                #                 </style>
-                #             </head>
-                #             <body>
-                #                 <img style="width: 100%%" src='data:%s;base64,%s'>
-                #             </body>
-                #             </html>
-                #         """ % (mime_type, image_data))
-                # self.web_view.setHtml(html)
-                # with open('test.html', 'w') as file:
-                #     file.write(html)
-                # self.web_view.show()
             elif mime_type.startswith("text"):
                 text = data.decode("utf-8", "ignore")
                 self.text_edit.setPlainText(text)
                 self.text_edit.show()
             elif mime_type == "application/pdf":
-                pdf_js_data = PDF_JS_MIN
-                pdf_worker_data = PDF_JS_MIN_WORKER
-                pdf_data_base64 = base64.b64encode(data).decode("utf-8")
-                html = """
-                        <!DOCTYPE html>
-                        <html>
-                        <head>
-                            <title>Gemino PDF Viewer - Based on PDF.js</title>
-                            <meta charset="UTF-8">
-                        </head>
-                        <body>
+                buffer = QtCore.QBuffer()
+                buffer.setData(QtCore.QByteArray(data))
+                buffer.open(QtCore.QIODevice.ReadOnly)
 
-                        </body>
-                        </html>
-                    """
-                self.page_content = """
-                        <script type="text/javascript" src="data:text/javascript;base64,%s" ></script>
-                        <style>
-                                #pdf-viewer {
-                                    width: 100%%;
-                                    height: 100%%;
-                                }
-                                .pdf-page {
-                                    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-                                    margin: 5px;
-                                }
-                        </style>
-                        <div id="pdf-viewer"></div>
-                        <script>
-                            const pdfData = "%s";
-                            const pdfUrl = "data:application/pdf;base64," + pdfData;
-                            pdfjsLib.GlobalWorkerOptions.workerSrc = 'data:application/javascript;base64,%s';
-                            pdfjsLib.getDocument(pdfUrl).promise.then(function(pdf) {
-                                const viewerContainer = document.getElementById('pdf-viewer');
-                                for (let i = 1; i <= pdf.numPages; i++) {
-                                    const page = document.createElement('div');
-                                    viewerContainer.appendChild(page);
-                                    pdf.getPage(i).then(function(pdfPage) {
-                                        const viewport = pdfPage.getViewport({ scale: 1 });
-                                        const canvas = document.createElement('canvas');
-                                        const context = canvas.getContext('2d');
-                                        canvas.height = viewport.height;
-                                        canvas.width = viewport.width;
-                                        canvas.classList.add('pdf-page');
-                                        page.appendChild(canvas);
-                                        pdfPage.render({
-                                            canvasContext: context,
-                                            viewport: viewport
-                                        });
-                                    });
-                                }
-                            });
-                        </script>
-                    """ % (
-                    pdf_js_data,
-                    pdf_data_base64,
-                    pdf_worker_data,
-                )
-                self.web_view.loadFinished.connect(self.load_view)
-
-                web_page = self.web_view.page()
-                web_page.setHtml(html)
-
-                self.web_view.show()
-
-            elif (
-                mime_type
-                == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-            ):
-                jszip_js_min = JSZIP_JS_MIN
-                docx_js_min = DOCX_JS_MIN
-                docx_data_base64 = base64.b64encode(data).decode("utf-8")
-                html = """
-                            <!DOCTYPE html>
-                            <html>
-                            <head>
-                                <title>Gemino DOCX Viewer - Based on docx.js</title>
-                                <meta charset="UTF-8">
-
-                            </head>
-                            <body>
-                                
-                            </body>
-                            </html>
-                        """
-                self.page_content = """
-                            <!DOCTYPE html>
-                            <html>
-                            <head>
-                                <title>Gemino DOCX Viewer - Based on docx.js</title>
-                                <meta charset="UTF-8">
-                                
-                            </head>
-                            <body>
-                                <div id="container"></div>
-                                <script type="text/javascript" src="data:text/javascript;base64,%s"></script>
-                                <script type="text/javascript" src="data:text/javascript;base64,%s"></script>
-                                <script>
-                                    const base64String = '%s';
-                                    
-                                    const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
-                                      const byteCharacters = atob(b64Data);
-                                      const byteArrays = [];
-
-                                      for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-                                        const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-                                        const byteNumbers = new Array(slice.length);
-                                        for (let i = 0; i < slice.length; i++) {
-                                          byteNumbers[i] = slice.charCodeAt(i);
-                                        }
-
-                                        const byteArray = new Uint8Array(byteNumbers);
-                                        byteArrays.push(byteArray);
-                                      }
-
-                                      const blob = new Blob(byteArrays, {type: contentType});
-                                      return blob;
-                                    }
-                                </script>
-                                <script>
-                                    async function parse_doc() {
-                                        const blob = b64toBlob(base64String, 'application/octet-stream');
-                                        docx.renderAsync(blob, document.getElementById("container"))
-                                            .then(x => console.log("docx: finished"));
-                                    }
-                                    parse_doc();
-                                </script>
-                            </body>
-                            </html>
-                        """ % (
-                    jszip_js_min,
-                    docx_js_min,
-                    docx_data_base64,
-                )
-                self.web_view.loadFinished.connect(self.load_view)
-
-                web_page = self.web_view.page()
-                web_page.setHtml(html)
-
-                self.web_view.show()
+                self.pdf_doc = QtPdf.QPdfDocument()
+                self.pdf_doc.load(buffer)
+                self.pdf_view.setDocument(self.pdf_doc)
+                self.pdf_view.show()
 
         except Exception as e:
             # Hide all widgets
             self.image_label.clear()
             self.text_edit.clear()
-            self.web_view.setHtml("")
             self.image_label.hide()
-            self.web_view.hide()
 
             self.text_edit.setPlainText(
                 f"Error loading file preview:\n{traceback.format_exc()}"
             )
             self.text_edit.show()
-
-    def load_view(self, finished=None):
-        web_page = self.web_view.page()
-        web_page.runJavaScript(f"document.write(`{self.page_content}`);")
-        self.page_content = ""
