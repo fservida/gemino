4e27508e3921a5296160360aaad34ccb094b1c60
Francesco Servida <git.fservida@gmail.com>
2024-04-28 00:31:32 +0300

    Initial windows MSIX bundler implementation

--- src/build/windows/windows_build_templates.py
+++ src/build/windows/windows_build_templates.py
@@ -1,8 +1,12 @@
 from jinja2 import Template
+import os
 
-VERSION = "2.8.0"
+VERSION = "2.9.0"
+VERSION_MSIX = "2.9.0.0"
 AUTHOR = "Francesco Servida"
 NAME = "gemino"
+DESCRIPTION = "Gemino File Duplicator"
+CWD = os.getcwd()
 
 with open('src/build/windows/version_info.py.j2') as src:
     with open('src/build/windows/version_info.py', "w") as dst:
@@ -11,3 +15,7 @@ with open('src/build/windows/version_info.py.j2') as src:
 with open('src/build/windows/Installer.nsi.j2') as src:
     with open('src/build/windows/Installer.nsi', "w") as dst:
         dst.write(Template(src.read()).render(version=VERSION, author=AUTHOR, app_name=NAME))
+
+with open('src/build/windows/msix_template.xml.j2') as src:
+    with open('src/build/windows/msix_template.xml', "w") as dst:
+        dst.write(Template(src.read()).render(version=VERSION, version_msix=VERSION_MSIX, author=AUTHOR, app_name=NAME, description=DESCRIPTION, cwd=CWD))
