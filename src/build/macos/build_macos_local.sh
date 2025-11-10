580667108d5ebf6e4b09fb4e01f9d14213f3774b
Francesco Servida <git.fservida@gmail.com>
2025-11-10 08:46:19 +0100

    Fix creation of app bundle for local development

--- src/build/macos/build_macos_local.sh
+++ src/build/macos/build_macos_local.sh
@@ -5,10 +5,6 @@ python src/build/macos/macos_build_templates.py
 rm -rf build/
 
 # Only use entitlements for app store version, non app store version is not sandboxed
-pyinstaller gemino.appstore.spec --workpath build/build --distpath build/dist/appstore -y
-cp embedded.provisionprofile build/dist/appstore/gemino.app/Contents/
-codesign --force --timestamp --verbose --options runtime --entitlements src/build/macos/entitlements.plist --sign '3rd Party Mac Developer Application: Francesco Servida (UVXFW83BXV)' build/dist/appstore/gemino.app
-
-rm -rf build/dist/appstore/gemino
-
-productbuild --sign "3rd Party Mac Developer Installer: Francesco Servida (UVXFW83BXV)" --component build/dist/appstore/gemino.app /Applications build/dist/gemino.pkg
+pyinstaller gemino.custom.spec --workpath build/build --distpath build/dist/app -y
+rm -rf build/dist/app/gemino
+rm -rf build/build
